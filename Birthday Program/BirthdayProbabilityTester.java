778fc638de6d21169695454f1dd76cfbdba9877f
ptrahan1713 <109534932+ptrahan1713@users.noreply.github.com>
2023-03-01 17:25:06 -0500

    Add files via upload

---
+++ Birthday Program/BirthdayProbabilityTester.java
@@ -0,0 +1,18 @@
+/**
+ * This program calls the Birthday Probability class to run the program. The 
+ * goal is to see what the probability of two people having the same birthday in
+ * the given group size and the given number of loops. Then outputs the percentage 
+ * of the given values.
+ * 
+ * @author Patrick Trahan
+ */
+public class BirthdayProbabilityTester
+{
+    
+    public static void main(String[] args)
+    {
+        BirthdayProbability birthday = new BirthdayProbability();
+        
+        birthday.run();
+    }
+}
